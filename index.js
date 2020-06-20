3c9dd52ad8b8a71fd4ad9fe9c365603329daa0a3
Sdavis <stevendavis0622@gmail.com>
2020-06-20 15:33:17 -0600

    setting up movie api

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+// b195b133
+// http://www.omdbapi.com/?apikey=[yourkey]&
+
+const fetchData = async () => {
+  const response = await axios.get(`http://www.omdbapi.com/`, {
+    params: {
+      apikey: `b195b133`,
+      s: `avengers`
+    }
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
