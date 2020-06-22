eb36eddfe97e4be2b81e63988c7202f6c3081845
Sdavis <stevendavis0622@gmail.com>
2020-06-21 22:54:57 -0600

    added input and debouncing

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 // b195b133
 // http://www.omdbapi.com/?apikey=[yourkey]&
 
-const fetchData = async () => {
+const fetchData = async searchTerm => {
   const response = await axios.get(`http://www.omdbapi.com/`, {
     params: {
       apikey: `b195b133`,
-      s: `avengers`
+      s: searchTerm
     }
   });
 
   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+
+let timeoutId;
+
+const onInput = event => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => fetchData(event.target.value), 1000);
+};
+
+input.addEventListener('input', onInput);
